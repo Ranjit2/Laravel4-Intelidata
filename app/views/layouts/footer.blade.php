bcae32e67cf86b6d820cb64120a4fe237540950e
Mazebeat <diego.pintod@gmail.com>
2014-08-12 09:40:19 -0400

    Nuevas vistas y complementos para el layout

---
+++ app/views/layouts/footer.blade.php
@@ -0,0 +1,54 @@
+<div class="col-md-6">
+	<div class="col-md-3">
+		<h5 class="text-uppercase"><strong>Productos</strong></h5>
+		<ul class="list-unstyled">
+			<li><a href="#">Producto 1</a></li>
+			<li><a href="#">Producto 1</a></li>
+			<li><a href="#">Producto 1</a></li>
+			<li><a href="#">Producto 1</a></li>
+			<li><a href="#">Producto 1</a></li>
+		</ul>
+	</div>
+	<div class="col-md-3">
+		<h5 class="text-uppercase"><strong>Servicios</strong></h5>
+		<ul class="list-unstyled">
+			<li><a href="#">Servicio 1</a></li>
+			<li><a href="#">Servicio 1</a></li>
+			<li><a href="#">Servicio 1</a></li>
+			<li><a href="#">Servicio 1</a></li>
+			<li><a href="#">Servicio 1</a></li>
+		</ul>
+	</div>
+	<div class="col-md-3">
+		<h5 class="text-uppercase"><strong>Empresa</strong></h5>
+		<ul class="list-unstyled">
+			<li><a href="#">Empresa 1</a></li>
+			<li><a href="#">Empresa 1</a></li>
+			<li><a href="#">Empresa 1</a></li>
+			<li><a href="#">Empresa 1</a></li>
+			<li><a href="#">Empresa 1</a></li>
+		</ul>
+	</div>
+	<div class="col-md-3">
+		<h5 class="text-uppercase"><strong>Contacto</strong></h5>
+		<ul class="list-unstyled">
+			<li><a href="#">LOREMIPSUM</a></li>
+			<li><a href="#">LOREMIPSUM</a></li>
+			<li><a href="#">LOREMIPSUM</a></li>
+			<li><a href="#">LOREMIPSUM</a></li>
+			<li><a href="#">LOREMIPSUM</a></li>
+		</ul>
+	</div>
+</div>
+<div class="col-md-3 social-icons">
+	<a href="#" class=""><span class='symbol'>circlefacebook</span><span class="sr-only">Facebook</span></a>
+	<a href="#" class=""><span class='symbol'>circletwitterbird</span><span class="sr-only">Twitter</span></a>
+	<a href="#" class=""><span class='symbol'>circleyoutube</span><span class="sr-only">Youtube</span></a>
+	<a href="#" class=""><span class='symbol'>circleinstagram</span><span class="sr-only">Instagram</span></a>
+</div>
+<div class="col-md-3 legal-terms">
+	<img src="holder.js/280x120" alt="" class="img-responsive">
+	<div class="clearfix"></div>
+	<strong>LOGO EMPRESA</strong>
+	<p class="muted">{{ Carbon::now()->year }} &copy; Intelidata. Todos los derechos reservados</p>
+</div>
---
+++ app/views/layouts/sidebar.blade.php
@@ -0,0 +1,60 @@
+<div class="panel-group" id="accordion">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a href="{{ URL::to('/') }}"><span class="icon fa fa-home fa-lg"></span>Home</a>
+			</h4>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="{{ URL::to('/') }}"><span class="icon fa fa-clock-o fa-lg"></span>Linea de Tiempo</a>
+			</h4>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><span class="icon fa fa-file-text-o fa-lg"></span>Reportes</a>
+			</h4>
+		</div>
+		<div id="collapseTwo" class="panel-collapse collapse">
+			<div class="panel-body">
+				<table class="table">
+					@if (Session::get('ses_user_tipo') === 'cliente')
+					<tr>
+						<td>
+							<a href="{{ URL::to('/charts/pie') }}"><span class="icon pe-7s-graph fa-lg"></span>Donut/Pie Chart</a>
+						</td>
+					</tr>
+					@endif
+					<tr>
+						<td>
+							<a href="{{ URL::to('/charts/column') }}"><span class="icon pe-7s-graph3 fa-lg"></span>Columnbar charts</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="{{ URL::to('/charts/stackbar') }}"><span class="icon pe-7s-graph2	 fa-lg"></span>Stackbar Chart</a>
+						</td>
+					</tr>
+					@if (Session::get('ses_user_tipo') === 'empresa')
+					<tr>
+						<td>
+							<a href="{{ URL::to('/charts/breakchart') }}"><span class="icon pe-7s-help2 fa-lg"></span>Break Chart</a>
+						</td>
+					</tr>
+					@endif
+				</table>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><span class="icon fa fa-envelope-o fa-lg"></span>Centro de Mensajes</a>
+			</h4>
+		</div>
+	</div>
+</div>
