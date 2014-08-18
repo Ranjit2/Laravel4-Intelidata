3c936a41b7ce42cdcc9abf31312a64ed1c65dff9
Mazebeat <diego.pintod@gmail.com>
2014-08-18 18:48:21 -0400

    20

---
+++ app/controllers/TimelineController.php
@@ -0,0 +1,27 @@
+	<?php
+
+	class TimelineController extends \BaseController {
+
+	/**
+	* Display a big timeline.
+	* GET /timeline
+	* POST-AJAX /timeline
+	*
+	* @return Response
+	*/
+	public function index()
+	{
+		if(Request::ajax() && Request::isMethod('POST')) {
+			$group_number = filter_var(Input::get('group_no', 0), FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH);
+			if(!is_numeric($group_number)){
+				header('HTTP/1.1 500 Invalid number!');
+				exit();
+			}
+			Telefono::tl_paginate($group_number);
+		} else {
+			$total_groups = Telefono::tl_total();
+			return View::make('timeline')->with('total_groups', $total_groups);
+		}
+	}
+
+}
